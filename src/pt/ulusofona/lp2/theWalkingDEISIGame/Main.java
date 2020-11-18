14d39f8758c7f66e940deeafe894f5e2c7ce50df
a21802650 <45458761+a21802650@users.noreply.github.com>
2020-11-18 11:02:29 +0000

    Add files via upload

---
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Main.java
@@ -0,0 +1,8 @@
+package pt.ulusofona.lp2.theWalkingDEISIGame;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Zombie.java
@@ -0,0 +1,27 @@
+package pt.ulusofona.lp2.theWalkingDEISIGame;
+
+public class Zombie {
+    int id;
+    int idTipo;
+    int posição;
+    int equipamentos;
+    String nomeEquipa;
+
+
+
+
+    public int getId(){
+        return 0;
+
+    }
+    public String getImagePNG() {
+        return null;
+
+    }
+    public String toString(){
+        return null;
+
+    }
+}
+
+

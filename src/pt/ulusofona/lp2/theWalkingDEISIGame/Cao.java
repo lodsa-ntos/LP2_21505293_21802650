64434213fc8a1c1c4562b5b5f575383dfa0951ec
Jolina Guvulo <a21802650@alunos.ulht.pt>
2020-12-15 14:53:13 +0000

    implementacao para novas creaturas e leitura do ficheiro

---
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Cao.java
@@ -0,0 +1,45 @@
+package pt.ulusofona.lp2.theWalkingDEISIGame;
+
+import java.util.ArrayList;
+
+public class Cao extends Creature {
+
+    public Cao(int id, int idTipo, String nome, int xAtual, int yAtual) {
+        super(id, idTipo, nome, xAtual, yAtual);
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int getIdEquipa() {
+        return idEquipa;
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public int getIdTipo() {
+        return idTipo;
+    }
+
+    @Override
+    public int getXAtual() {
+        return xAtual;
+    }
+
+    @Override
+    public int getYAtual() {
+        return yAtual;
+    }
+
+    @Override
+    public ArrayList<Equipamento> getEquipamentos() {
+        return null;
+    }
+}
---
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Creature.java
@@ -0,0 +1,110 @@
+package pt.ulusofona.lp2.theWalkingDEISIGame;
+
+import java.util.ArrayList;
+
+public abstract class Creature {
+    int id;
+    int idTipo;
+    int idEquipa = 10;
+    int idEquipaZombie = 20;
+    String nome;
+
+    String tipo;
+    String equipa;
+    boolean isAlive;
+
+    int xAtual;
+    int yAtual;
+    int xAnterior;
+    int yAnterior;
+
+    int nrHumanos = 0;
+    int nrZombies = 0;
+    ArrayList<Equipamento> equipamentos = new ArrayList<>();
+    ArrayList<Equipamento> destruidos = new ArrayList<>();
+
+    public Creature(int id, int idTipo, String nome, int xAtual, int yAtual) {
+        this.id = id;
+        this.idTipo = idTipo;
+        this.nome = nome;
+        this.xAtual = xAtual;
+        this.yAtual = yAtual;
+    }
+
+    // Devolve o ID da criatura.
+    public abstract int getId();
+
+    public abstract int getIdEquipa();
+
+    public abstract String getNome();
+
+    public abstract int getIdTipo();
+
+    public abstract int getXAtual();
+
+    public abstract int getYAtual();
+
+    public abstract ArrayList<Equipamento> getEquipamentos();
+
+    public void setxAtual(int xAtual) {}
+
+    public void setyAtual(int yAtual) {}
+
+    public void contarHumanos(int nrHumanos) {}
+
+    public void contarZombies (int nrZombies) {}
+
+    // Metodo onde <Tipo> corresponde aos nomes (p.e. “Humano” ou “Zombie”).
+    public void getTipo(){
+        if(this.idTipo == 1){
+            tipo = "Adulto (Zombie)";
+        } else if(this.idTipo == 6) {
+            tipo = "Adulto (Vivo)";
+        } else {
+            tipo = "Cão";
+        }
+    }
+    // Metodo onde <Equipa> corresponde aos nomes (p.e. “Os Vivos” ou “Os Outros”).
+    public void getEquipa() {
+        if(this.idEquipa == 10){
+            equipa = "Os Vivos";
+        }
+    }
+    public void getEquipaZ(){
+        if (this.idEquipaZombie == 20){
+            equipa = "Os Outros";
+        }
+    }
+
+
+    // Metodo que devolve o nome do ficheiro de imagem (formato PNG) que representa a criatura.
+    public String getImagePNG() {
+        if(this.id == 1){
+            return new String("idoso.png");
+        } else if(this.id == 2) {
+            return new String("jackie.png");
+        } else if(this.id == 3) {
+            return new String("alice.png");
+        } else if(this.id == 4) {
+            return new String("ash.png");
+        } else if(this.id == 5) {
+            return new String("dog.png");
+        } else if(this.id == 6) {
+            return new String("zomb.png");
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (equipa.equals("Os Vivos")){
+            return id + " | " + tipo + " | " + equipa + " | " + nome + " " + equipamentos.size() + " @ (" + xAtual + ", " + yAtual + ")";
+        } else if (equipa.equals("Os Outros")){
+            return id + " | " + tipo + " | " + equipa + " | " + nome + " " + destruidos.size() + " @ (" + xAtual + ", " + yAtual + ")";
+        } else {
+            return id + " | " + tipo + " | " + equipa + " | " + nome + " @ (" + xAtual + ", " + yAtual + ")";
+        }
+    }
+
+}

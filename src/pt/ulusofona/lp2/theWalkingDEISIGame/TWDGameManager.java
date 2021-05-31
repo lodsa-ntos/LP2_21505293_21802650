7d73eaf694189a162f6321b36f5c8a632fb5a1d1
Lodney Santos <a21505293@alunos.ulht.pt>
2021-05-31 02:25:26 +0100

    correcao:  - ZombiesDetroiemMuitosEquipamentos (test17 e test18)

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -1242,28 +1242,6 @@ public class TWDGameManager {
             case 5:
             case 8:
             case 9:
-            case 12:
-            case 13:
-            case 16:
-            case 17:
-            case 20:
-            case 21:
-            case 24:
-            case 25:
-            case 28:
-            case 29:
-            case 32:
-            case 33:
-            case 36:
-            case 37:
-            case 40:
-            case 41:
-            case 44:
-            case 45:
-            case 48:
-            case 49:
-            case 52:
-            case 53:
                 diurno = true;
                 break;
             case 2:
@@ -1272,26 +1250,6 @@ public class TWDGameManager {
             case 7:
             case 10:
             case 11:
-            case 14:
-            case 15:
-            case 18:
-            case 19:
-            case 22:
-            case 23:
-            case 26:
-            case 27:
-            case 30:
-            case 31:
-            case 34:
-            case 35:
-            case 38:
-            case 39:
-            case 42:
-            case 43:
-            case 46:
-            case 47:
-            case 50:
-            case 51:
                 diurno = false;
                 break;
         }

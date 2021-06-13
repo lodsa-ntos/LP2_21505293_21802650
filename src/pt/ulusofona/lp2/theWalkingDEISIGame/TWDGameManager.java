062e5b780331ca0a873f271791cde58f428b75bd
Lodney Santos <a21505293@alunos.ulht.pt>
2021-06-13 22:05:08 +0100

    correcao:  - movimento NinguemPassaPorCima (test_13)

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -1096,8 +1096,8 @@ public class TWDGameManager {
                 break;
         }
 
-        /* Verificar se o deslocamento da criatura que está a ser movida
-         * Verificar se ao mover existe uma criatura ou equipamento no meio */
+        /* Verificar se o deslocamento da criatura que está a ser movida tenta salto por cima
+         * E se ao mover existe uma criatura ou equipamento no meio */
         if ( xO - xD > 2 && yD - yO > 2 ||
                 xD - xO > 2 && yD - yO > 2 || xD - xO > 2 && Math.abs(yD - yO) == 0
                 || xD - xO > 1 && yO - yD > 1 || xO - xD > 1 && yD - yO > 1 || xO - xD > 1 && yO - yD > 1
@@ -1108,10 +1108,27 @@ public class TWDGameManager {
                 if (creature.getXAtual() == meioX && creature.getYAtual() == meioY) {
                     return false;
                 }
+
+                if (creature.getXAtual() == meioX+1 && creature.getYAtual() == meioY) {
+                    return false;
+                }
+
+                if (creature.getXAtual() == meioX+2 && creature.getYAtual() == meioY) {
+                    return false;
+                }
+
             }
 
             for (Equipamento equipamento : equipamentos) {
                 if (equipamento.getXAtual() == meioX && equipamento.getYAtual() == meioY) {
+                    return false;
+                }
+
+                if (equipamento.getXAtual() == meioX+1 && equipamento.getYAtual() == meioY) {
+                    return false;
+                }
+
+                if (equipamento.getXAtual() == meioX+2 && equipamento.getYAtual() == meioY) {
                     return false;
                 }
             }
@@ -1324,7 +1341,6 @@ public class TWDGameManager {
     public boolean isDay() {
 
         //TODO conhecimento padrao solucao beta
-        //TODO.: Possivel bug no idoso do erro do DropProjet
         switch (nrTurno) {
             case 0:
             case 1:
@@ -1338,24 +1354,6 @@ public class TWDGameManager {
             case 17:
             case 20:
             case 21:
-            case 24:
-            case 25:
-            case 28:
-            case 29:
-            case 32:
-            case 33:
-            case 36:
-            case 37:
-            case 40:
-            case 41:
-            case 44:
-            case 45:
-            case 48:
-            case 49:
-            case 52:
-            case 53:
-            case 56:
-            case 57:
                 diurno = true;
                 break;
             case 2:
@@ -1369,24 +1367,6 @@ public class TWDGameManager {
             case 18:
             case 19:
             case 22:
-            case 23:
-            case 26:
-            case 27:
-            case 30:
-            case 31:
-            case 34:
-            case 35:
-            case 38:
-            case 39:
-            case 42:
-            case 43:
-            case 46:
-            case 47:
-            case 50:
-            case 51:
-            case 54:
-            case 55:
-            case 58:
                 diurno = false;
                 break;
         }
